fe63ae235139dbd9e49128997d036e606ace1fb2
Lewis Dicks <lewis_dicks@live.co.uk>
2015-04-21 19:10:48 +0100

    Update List.java
    added alternative approaches with for loops

--- 2013/DuncansAns/List.java
+++ 2013/DuncansAns/List.java
@@ -13,12 +13,12 @@ public class List<E extends Comparable<E>> {
 	public boolean equals(List<E> L){
 		Node<E> p1 = head;
 		Node<E> p2 = L.getHead();
+		if (size != L.size) return false;
 		while(p1 != null && p2 != null){
 			if(!p1.equals(p2)){return false;}
 			p1 = p1.getNext();
 			p2 = p2.getNext();
 		}
-		if(p1 != p2){return false;}
 		return true;
 	}
 	public boolean isPresent(E s){
@@ -29,12 +29,32 @@ public class List<E extends Comparable<E>> {
 		}
 		return false;
 	}
+	
+	// alternative by Lewis
+	
+	public boolean isPresent(E s){
+		for(Node<E> pointer = head; pointer !=null; pointer=pointer.getNext(){
+			if (pointer.getElement().equals(s)) return true;
+		}
+		return false;
+	}
+	
 	public List<E> intersect (List<E> L){
 		List<E> returnList = new List<E>();
 		Node<E> pointer = head;
 		while (pointer != null){
-			if(L.isPresent(pointer.getElement())){L.addFront(pointer.getElement());}
+			if(L.isPresent(pointer.getElement())){returnList.addFront(pointer.getElement());}
 			pointer = pointer.getNext();
+		}
+		return returnList;
+	}
+	
+	// alternative by Lewis
+	
+	public List<E> intersect (List<E> L){
+		List<E> returnList = new List<E>();
+		for(Node<E> pointer = head; pointer !=null; pointer=pointer.getNext(){
+			if(L.isPresent(pointer.getElement()))returnList.addFront(pointer.getElement());
 		}
 		return returnList;
 	}

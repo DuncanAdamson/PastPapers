d41eb06e22f12a78a6c126ca8f22f4a4b0176e9b
Duncan Adamson <2073411a@student.gla.ac.uk>
2015-04-22 18:34:24 +0100

    Update BSTree.java

--- 2012/DuncansAns/BSTree.java
+++ 2012/DuncansAns/BSTree.java
@@ -29,7 +29,8 @@ public class BSTree {
 		return subsumed(root,t);
 	}
 	public boolean subsumed(BNode bn, BSTree t){
-		if(bn == null) return false;
+		if(t == null & bn != null) return false;
+		if(bn == null) return true; // Waiting for it to reach the end of each node
 		return t.isPresent(bn.getItem()) && subsumed(bn.getLeft(),t) && subsumed(bn.getRight(),t);
 		
 	}

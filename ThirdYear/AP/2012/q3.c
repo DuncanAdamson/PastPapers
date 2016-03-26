e32c1416bd86a1c7f751709d01f17c2cd451591a
Delagon <duncanadamson@mail.com>
2016-03-26 10:49:08 +0000

    OS 2014

--- ThirdYear/AP/2012/q3.c
+++ ThirdYear/AP/2012/q3.c
@@ -1,3 +1,4 @@
+/*
 3a)
 '&' bitwise and
 '|' bitwise or
@@ -6,8 +7,10 @@
 '>>' right shift
 '~' not
 b) i)
+
 a >> N
 reason: as b is 2 to the power of N, shifting to the right by N is the equivelent of deviding by b.
 ii)
 (b - 1) & a
 reason: b - 1 is equivlent to a string of 1's N long when b is 2 to the power of N, therefore the bitwise and operation will return the value of the last N digits of a, equivlent to a % b.
+*/
---
+++ ThirdYear/OS/readme.txt
@@ -0,0 +1,1 @@
+Answers are presented in each section(A,B<C) for OS past papers in .txt files
